a4d899960dbe3ec71ac596ed16e7a19e67dd3b5d
zehranisar <zehranisar@users.noreply.github.com>
2026-04-12 14:00:00 +0500

    Add expression precedence parsing rules

--- parser.py
+++ parser.py
@@ -104,3 +104,73 @@ class Parser:
 
     def _stmt(self):
         raise NotImplementedError("Statement parsing coming soon")
+
+    # Expression parsing rules (operator precedence)
+    def _expr(self):       return self._or_expr()
+
+    def _or_expr(self):
+        node = self._and_expr()
+        while self._check(TT.OR):
+            op = self._advance().type
+            node = BinOpNode(op, node, self._and_expr())
+        return node
+
+    def _and_expr(self):
+        node = self._eq_expr()
+        while self._check(TT.AND):
+            op = self._advance().type
+            node = BinOpNode(op, node, self._eq_expr())
+        return node
+
+    def _eq_expr(self):
+        node = self._rel_expr()
+        while self._check(TT.EQ, TT.NEQ):
+            op = self._advance().type
+            node = BinOpNode(op, node, self._rel_expr())
+        return node
+
+    def _rel_expr(self):
+        node = self._add_expr()
+        while self._check(TT.LT, TT.GT, TT.LTE, TT.GTE):
+            op = self._advance().type
+            node = BinOpNode(op, node, self._add_expr())
+        return node
+
+    def _add_expr(self):
+        node = self._mul_expr()
+        while self._check(TT.PLUS, TT.MINUS):
+            op = self._advance().type
+            node = BinOpNode(op, node, self._mul_expr())
+        return node
+
+    def _mul_expr(self):
+        node = self._unary()
+        while self._check(TT.STAR, TT.SLASH, TT.PERCENT):
+            op = self._advance().type
+            node = BinOpNode(op, node, self._unary())
+        return node
+
+    def _unary(self):
+        if self._check(TT.NOT, TT.MINUS):
+            op = self._advance().type
+            return UnaryOpNode(op, self._unary())
+        return self._primary()
+
+    def _primary(self):
+        tok = self._peek()
+        if tok.type == TT.INTEGER:
+            self._advance(); return LiteralNode(tok.value, "int")
+        if tok.type == TT.FLOAT:
+            self._advance(); return LiteralNode(tok.value, "float")
+        if tok.type == TT.STRING:
+            self._advance(); return LiteralNode(tok.value, "string")
+        if tok.type in (TT.TRUE, TT.FALSE):
+            self._advance(); return LiteralNode(tok.value, "bool")
+        if tok.type == TT.IDENT:
+            self._advance(); return IdentNode(tok.value)
+        if tok.type == TT.LPAREN:
+            self._advance()
+            node = self._expr()
+            self._expect(TT.RPAREN)
+            return node
+        raise ParseError(f"Unexpected token {tok.type!r} at line {tok.line}, col {tok.col}")
